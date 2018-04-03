7f811af6bcc40d51a4da2058f673e9f0b04114c9
李梦龙 <649076408@qq.com>
2018-04-04 01:07:29 +0800

    feature: Compress and optimize  image

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,31 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader'
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: '65-90',
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          },
         ]
       }
     ]
