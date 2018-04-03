f12aac604fa0b30d7025d131fc9ebc5a1609543e
李梦龙 <649076408@qq.com>
2018-04-03 23:41:40 +0800

    feature: load img

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import join from 'lodash/join';
 import './style.css';
+import Icon from './icon.jpg';
 
 function component() {
   var element = document.createElement('div');
@@ -7,6 +8,12 @@ function component() {
   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
   element.innerHTML = join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  // 将图像添加到我们现有的 div。
+  var myIcon = new Image();
+  myIcon.src = Icon;
+
+  element.appendChild(myIcon);
 
   return element;
 }
